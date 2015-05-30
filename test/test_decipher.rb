79ac5934b908211dab65be95116707cebc62bc81
Hanes Barbosa <hanes.barbosa@gmail.com>
2015-05-30 12:48:04 -0300

    #3 Transforming tabs to spaces.

--- test/test_decipher.rb
+++ test/test_decipher.rb
@@ -4,26 +4,26 @@ require_relative './support/test_data'
 
 class TestDecipher < Test::Unit::TestCase
 
-	def setup
-		@decipher = Alnum::Decipher.new		
-	end
+  def setup
+    @decipher = Alnum::Decipher.new   
+  end
 
-	def test_one_character_range
-		62.times do |number|
-			assert_equal @decipher.read(SYMBOLS[number]), number
-		end
-	end
+  def test_one_character_range
+    62.times do |number|
+      assert_equal @decipher.read(SYMBOLS[number]), number
+    end
+  end
 
-	def test_character_out_of_range
-		assert_raise RangeError do
-			@decipher.read('-')
-		end
-	end
+  def test_character_out_of_range
+    assert_raise RangeError do
+      @decipher.read('-')
+    end
+  end
 
-	def test_empty_arguments
-		assert_raise ArgumentError do
-			@decipher.read()
-		end
-	end
+  def test_empty_arguments
+    assert_raise ArgumentError do
+      @decipher.read()
+    end
+  end
 
 end
