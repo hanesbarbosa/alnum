7127dbc4d19ddcbf078f3dc2d67f5367afd69087
Hanes Barbosa <hanes.barbosa@gmail.com>
2015-05-31 01:15:19 -0300

    #7 Fixing typing error.

--- test/test_cypher.rb
+++ test/test_cypher.rb
@@ -5,19 +5,19 @@ class TestCypher < Test::Unit::TestCase
 
   def test_one_character_range
     62.times do |number|
-      assert_equal Alnum::cypher(number), "#{SYMBOLS[number]}"
+      assert_equal Alnum.cypher(number), "#{SYMBOLS[number]}"
     end
   end
 
   def test_only_integers_allowed
     assert_raise TypeError do
-      Alnum::cypher('a')
+      Alnum.cypher('a')
     end   
   end
 
   def test_empty_arguments
     assert_raise ArgumentError do
-      Alnum::cypher()
+      Alnum.cypher()
     end
   end
 
--- test/test_decipher.rb
+++ test/test_decipher.rb
@@ -5,19 +5,19 @@ class TestDecipher < Test::Unit::TestCase
 
   def test_one_character_range
     62.times do |number|
-      assert_equal Alnum::decipher(SYMBOLS[number]), number
+      assert_equal Alnum.decipher(SYMBOLS[number]), number
     end
   end
 
   def test_character_out_of_range
     assert_raise RangeError do
-      Alnum::decipher('-')
+      Alnum.decipher('-')
     end
   end
 
   def test_empty_arguments
     assert_raise ArgumentError do
-      Alnum::decipher()
+      Alnum.decipher()
     end
   end
 
